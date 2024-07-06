4bcd4fbc48cfd2adc35f308518381ae632c1187c
ShaswataG <gshaswata1@gmail.com>
2024-07-06 23:25:20 +0530

    slider animation

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
     gsap.registerPlugin(ScrollTrigger);
 
+    const svg = document.getElementById("transring");
+
+    const svgNodes = Array.from(svg.querySelectorAll(".dots-nav"));
+
+    function highlightNode(index) {
+        console.log('highlightNode called',index)
+
+        // highlight path
+        const transparentRing = document.querySelector("#Transparent_Ring");
+        const pathLength = transparentRing.getTotalLength();
+        const start = pathLength * (index / 7);
+        const end = pathLength * ((index + 1) / 7);
+
+        transparentRing.style.strokeDasharray = `${end - start} ${pathLength}`;
+        transparentRing.style.strokeDashoffset = start;
+        transparentRing.style.stroke = "red";
+
+        // highlight nodes
+        svgNodes.forEach((node,nodeIndex) => {
+            const fill = node.querySelector('.dotsst')
+            const border = node.querySelector('.dotsstro')
+            if (nodeIndex <= index) {
+                TweenMax.to(fill, 0.5, {fill: "#0092ff", opacity: 1})
+                TweenMax.to(border, 0.5, {fill: "none", opacity: 1})
+            } else {
+                TweenMax.to(fill, 0.5, {fill: "#fff", opacity: 0.5})
+                TweenMax.to(border, 0.5, {opacity: 0})
+            }
+        });
+    }
+
     objLeft.forEach((org, index) => {
         gsap.fromTo(
             "#box-left",
@@ -17,15 +48,21 @@ $(document).ready(function() {
                 },
                 backgroundColor: org.color,
                 onStart: () => {
+                    highlightNode(index)
+                    $('#Opaque_Ring').css("fill", org.color)
                     $(`#organisation-info`).html(org.orgInfo);
                 },
+                onComplete: ()=> {
+
+                },
                 onReverseComplete: () => {
+                    highlightNode(index - 1)
                     if (index > 0) {
                         $(`#organisation-info`).html(objLeft[index - 1].orgInfo);
                     } else {
                         $(`#organisation-info`).html('');
                     }
-                }
+                },
             }
         );
     });
@@ -38,7 +75,6 @@ $(document).ready(function() {
                 scrollTrigger: {
                     trigger: `#right-${index + 1}`,
                     start: "top center",
-                    // end: "top center",
                     end: "bottom bottom",
                     scrub: true,
                     toggleActions: "play reverse play reverse"
@@ -57,23 +93,5 @@ $(document).ready(function() {
             }
         );
     });
-
-    // colorsRight.forEach((color, index) => {
-    // 	gsap.fromTo(
-    // 		"#box-right",
-    // 		{ backgroundColor: colorsRight[index - 1] || "#CCCCCE" },
-    // 		{
-    // 			scrollTrigger: {
-    // 				trigger: `#right-${index-1}`,
-    // 				start: "top center",
-    // 				// end: "top center",
-    // 				end: "bottom bottom",
-    // 				scrub: true,
-    // 				toggleActions: "play reverse play reverse"
-    // 			},
-    // 			backgroundColor: color
-    // 		}
-    // 	)
-    // });
 });
 
