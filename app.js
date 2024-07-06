f646c3ba5f9aeea582cfef304f52414b6bdb1e4d
ShaswataG <gshaswata1@gmail.com>
2024-07-06 12:21:26 +0530

    image animations

--- app.js
+++ app.js
@@ -1,244 +1,79 @@
-const occupationInfoDiv = document.querySelector("#organisation-info");
+$(document).ready(function() {
+    gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger);
-
-const colorsLeft = [
-    "#561DB9",
-    "#3B21A0",
-    "#0E123A",
-    "#16263B",
-    "#124CA1",
-    "#012C91",
-    "#00834C"
-];
-
-const objLeft = [
-    {
-        color: "#561DB9",
-        orgInfo: `
-					<div id="left-1">
-						<div>
-							<div class='first-organisation-award'>
-								<img src=''/>
-								<div>
-									<span>Best Digital Experience</span>
-									<span>World Communication Awards</span>
-								</div>
-							</div>
-							<div class='organisation-info--title'>
-								<h1>ABC 123</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best web development</span>
-								<span>company in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<a href="#">View case study</a>
-							<img src='./assets/arrow-right.svg' />
-						</div>
-					</div>`
-    },
-    {
-        color: "#3B21A0",
-        orgInfo: `
-					<div id="left-2">
-						<div>
-							<div class='organisation-info--title'>
-								<h1>ABC 234</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best web development</span>
-								<span>company in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<span>Coming soon</span>
-						</div>
-					</div>`
-    },
-    {
-        color: "#0E123A",
-        orgInfo: `
-					<div id="left-3">
-						<div>
-							<div class='organisation-info--title'>
-								<h1>ABC 456</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best</span>
-								<span>web development company</span>
-								<span>in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<a href="#">View case study</a>
-							<img src='./assets/arrow-right.svg' />
-						</div>
-					</div>`
-    },
-    {
-        color: "#16263B",
-        orgInfo: `
-					<div id="left-4">
-						<div>
-							<div class='organisation-info--title'>
-								<h1>ABC 456</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best</span>
-								<span>web development company</span>
-								<span>in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<a href="#">View case study</a>
-							<img src='./assets/arrow-right.svg' />
-						</div>
-					</div>
-				`
-    },
-    {
-        color: "#124CA1",
-        orgInfo: `
-					<div id="left-5">
-						<div>
-							<div class='organisation-info--title'>
-								<h1>ABC 567</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best AR</span>
-								<span>Development company</span>
-								<span>in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<a href="#">View case study</a>
-							<img src='./assets/arrow-right.svg' />
-						</div>
-					</div>
-				`
-    },
-    {
-        color: "#012C91",
-        orgInfo: `
-					<div id="left-6">
-						<div>
-							<div class='organisation-info--title'>
-								<h1>ABC 678</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>Best since 2017</span>
-								<span>We offer wide range of</span>
-								<span>web development and app development</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<a href="#">View case study</a>
-							<img src='./assets/arrow-right.svg' />
-						</div>
-					</div>
-				`
-    },
-    {
-        color: "#00834C",
-        orgInfo: `
-					<div id="left-7">
-						<div>
-							<div class='last-organisation-award'>
-								<img src=''/>
-								<div>
-									<span>Mobile App of the Year</span>
-									<span>Entrepreneur</span>
-								</div>
-							</div>
-							<div class='organisation-info--title'>
-								<h1>ABC 23478</h1>
-							</div>
-							<div class='organisation-info--body'>
-								<span>We are the best</span>
-								<span>web development company</span>
-								<span>in the world</span>
-							</div>
-						</div>
-						<div class='organisation-info--remark'>
-							<span>Coming soon</span>
-						</div>
-					</div>`
-    }
-]
-
-const colorsRight = [
-    "#CCCCCE",
-    "#1C0362",
-    "#141748",
-    "#0D131B",
-    "#28C9D8",
-    "#000000",
-    "#00B769"
-];
-
-objLeft.forEach((org, index) => {
-    gsap.fromTo(
-        "#box-left",
-        {
-            backgroundColor: objLeft[index - 1] ? objLeft[index - 1].color : "#561DB9",
-        },
-        {
-            scrollTrigger: {
-                trigger: `#left-${index + 1}`,
-                start: "top center",
-                end: "bottom bottom",
-                scrub: true,
-                toggleActions: "play reverse play reverse",
+    objLeft.forEach((org, index) => {
+        gsap.fromTo(
+            "#box-left",
+            {
+                backgroundColor: objLeft[index - 1] ? objLeft[index - 1].color : "#561DB9",
             },
-            backgroundColor: org.color,
-            onStart: () => {
-                $(`#organisation-info`).html(org.orgInfo);
-            },
-            onReverseComplete: () => {
-                if (index > 0) {
-                    $(`#organisation-info`).html(objLeft[index - 1].orgInfo);
-                } else {
-                    $(`#organisation-info`).html('');
+            {
+                scrollTrigger: {
+                    trigger: `#left-${index + 1}`,
+                    start: "top center",
+                    end: "bottom bottom",
+                    scrub: true,
+                    toggleActions: "play reverse play reverse",
+                },
+                backgroundColor: org.color,
+                onStart: () => {
+                    $(`#organisation-info`).html(org.orgInfo);
+                },
+                onReverseComplete: () => {
+                    if (index > 0) {
+                        $(`#organisation-info`).html(objLeft[index - 1].orgInfo);
+                    } else {
+                        $(`#organisation-info`).html('');
+                    }
                 }
             }
-        }
-    );
+        );
+    });
+
+    objRight.forEach((org, index) => {
+        gsap.fromTo(
+            "#box-right",
+            { backgroundColor: objRight[index - 1] ? objRight[index - 1].color : "#CCCCCE" },
+            {
+                scrollTrigger: {
+                    trigger: `#right-${index + 1}`,
+                    start: "top center",
+                    // end: "top center",
+                    end: "bottom bottom",
+                    scrub: true,
+                    toggleActions: "play reverse play reverse"
+                },
+                backgroundColor: org.color,
+                onStart: () => {
+                    $(`#organisation-images`).html(org.orgImg);
+                },
+                onReverseComplete: () => {
+                    if (index > 0) {
+                        $(`#organisation-images`).html(objRight[index - 1].orgImg);
+                    } else {
+                        $(`#organisation-images`).html('');
+                    }
+                }
+            }
+        );
+    });
+
+    // colorsRight.forEach((color, index) => {
+    // 	gsap.fromTo(
+    // 		"#box-right",
+    // 		{ backgroundColor: colorsRight[index - 1] || "#CCCCCE" },
+    // 		{
+    // 			scrollTrigger: {
+    // 				trigger: `#right-${index-1}`,
+    // 				start: "top center",
+    // 				// end: "top center",
+    // 				end: "bottom bottom",
+    // 				scrub: true,
+    // 				toggleActions: "play reverse play reverse"
+    // 			},
+    // 			backgroundColor: color
+    // 		}
+    // 	)
+    // });
 });
 
-colorsRight.forEach((color, index) => {
-    gsap.fromTo(
-        "#box-right",
-        { backgroundColor: colorsRight[index - 1] || "#CCCCCE" },
-        {
-            scrollTrigger: {
-                trigger: `#right-${index + 1}`,
-                start: "top center",
-                // end: "top center",
-                end: "bottom bottom",
-                scrub: true,
-                toggleActions: "play reverse play reverse"
-            },
-            backgroundColor: color
-        }
-    );
-});
-
-// colorsRight.forEach((color, index) => {
-// 	gsap.fromTo(
-// 		"#box-right",
-// 		{ backgroundColor: colorsRight[index - 1] || "#CCCCCE" },
-// 		{
-// 			scrollTrigger: {
-// 				trigger: `#right-${index-1}`,
-// 				start: "top center",
-// 				// end: "top center",
-// 				end: "bottom bottom",
-// 				scrub: true,
-// 				toggleActions: "play reverse play reverse"
-// 			},
-// 			backgroundColor: color
-// 		}
-// 	)
-// });
---
+++ constants.js
@@ -0,0 +1,235 @@
+const colorsLeft = [
+    "#561DB9",
+    "#3B21A0",
+    "#0E123A",
+    "#16263B",
+    "#124CA1",
+    "#012C91",
+    "#00834C"
+];
+
+const objLeft = [
+    {
+        color: "#561DB9",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='first-organisation-award'>
+								<img src=''/>
+								<div>
+									<span>Best Digital Experience</span>
+									<span>World Communication Awards</span>
+								</div>
+							</div>
+							<div class='organisation-info-title'>
+								<h1>ABC 123</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best web development</span>
+								<span>company in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<a href="#">View case study</a>
+							<img src='./assets/arrow-right.svg' />
+						</div>
+					</div>`
+    },
+    {
+        color: "#3B21A0",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='organisation-info-title'>
+								<h1>ABC 234</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best web development</span>
+								<span>company in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<span>Coming soon</span>
+						</div>
+					</div>`
+    },
+    {
+        color: "#0E123A",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='organisation-info-title'>
+								<h1>ABC 456</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best</span>
+								<span>web development company</span>
+								<span>in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<a href="#">View case study</a>
+							<img src='./assets/arrow-right.svg' />
+						</div>
+					</div>`
+    },
+    {
+        color: "#16263B",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='organisation-info-title'>
+								<h1>ABC 456</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best</span>
+								<span>web development company</span>
+								<span>in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<a href="#">View case study</a>
+							<img src='./assets/arrow-right.svg' />
+						</div>
+					</div>
+				`
+    },
+    {
+        color: "#124CA1",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='organisation-info-title'>
+								<h1>ABC 567</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best AR</span>
+								<span>Development company</span>
+								<span>in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<a href="#">View case study</a>
+							<img src='./assets/arrow-right.svg' />
+						</div>
+					</div>
+				`
+    },
+    {
+        color: "#012C91",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='organisation-info-title'>
+								<h1>ABC 678</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>Best since 2017</span>
+								<span>We offer wide range of</span>
+								<span>web development and app development</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<a href="#">View case study</a>
+							<img src='./assets/arrow-right.svg' />
+						</div>
+					</div>
+				`
+    },
+    {
+        color: "#00834C",
+        orgInfo: `
+					<div class="left-content">
+						<div>
+							<div class='last-organisation-award'>
+								<img src=''/>
+								<div>
+									<span>Mobile App of the Year</span>
+									<span>Entrepreneur</span>
+								</div>
+							</div>
+							<div class='organisation-info-title'>
+								<h1>ABC 23478</h1>
+							</div>
+							<div class='organisation-info-body'>
+								<span>We are the best</span>
+								<span>web development company</span>
+								<span>in the world</span>
+							</div>
+						</div>
+						<div class='organisation-info-remark'>
+							<span>Coming soon</span>
+						</div>
+					</div>`
+    }
+]
+
+const objRight = [
+    {
+        color: "#CCCCCE",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/nexgtv-entertainment-mobile-app-development.png" alt="nexgtv-entertainment" />
+                        <img src="assets/nexgtv-mobile-app-ui-design.png" alt="nexgtv-mobile-app" />
+                    </div>`
+    },
+    {
+        color: "#1C0362",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/veme-blockchain-app-developed.png" alt="veme-blockchain-app" />
+                        <img src="assets/veme-app-ui-design.png" alt="veme-app-ui-design" />
+                    </div>`
+    },
+    {
+        color: "#141748",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/nasa-fitness-tracking-mobile-app.png" alt="nasa-fitness-tracking-mobile-app" />
+                        <img src="assets/measure-total-body-weight-through-fitness-app.png" alt="measure-total-body-weight-through-fitness-app" />
+                    </div>`
+    },
+    {
+        color: "#0D131B",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/pizza_box.png" alt="pizza_box" />
+                        <img src="assets/domi-img1.png" alt="dominos" />
+                        <img src="assets/ux-strategy-for-mobile-app-devlopment.png" alt="ux-strategy" />
+                    </div>`
+    },
+    {
+        color: "#28C9D8",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/karavan-social-networking-app-screen.png" alt="karavan-social-networking" />
+                        <img src="assets/social-networking-app-case-study.png" alt="social-networking-app" />
+                        <img src="assets/developers-for-social-media-app.png" alt="developers-for-social-media-app" />
+                    </div>`
+    },
+    {
+        color: "#000000",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/erp-app-development-service.png" alt="erp-app-development-service" />
+                    </div>`
+    },
+    {
+        color: "#00B769",
+        orgImg: `
+                    <div class="right-content">
+                        <img src="assets/melltoo-img2.png" alt="melltoo-img2" />
+                        <img src="assets/melltoo-img1.png" alt="melltoo-img1" />
+                    </div>`
+    }
+]
+
+const colorsRight = [
+    "#CCCCCE",
+    "#1C0362",
+    "#141748",
+    "#0D131B",
+    "#28C9D8",
+    "#000000",
+    "#00B769"
+];
